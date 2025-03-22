40e89e17e7dccfe5a62aaf492c8127eeeb5a6513
ParySharma <parth9512158@gmail.com>
2025-03-22 13:59:39 +0530

    New 101

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ const nextConfig = {
   reactStrictMode: false,
   distDir: 'build', // Custom build directory instead of `.next`
   output: 'standalone', // Standalone mode for SSR & dynamic routes
-  experimental: {
-    appDir: false, // Fix potential issues with standalone mode
-  },
+  // experimental: {
+  //   appDir: false, // Fix potential issues with standalone mode
+  // },
   trailingSlash: true, // Enable trailing slash for all URLs
   images: {
     unoptimized: true, // Required if using Next.js Image component
