2a512cb1315b410b486cac6a8606b7af3fe5ddeb
ParySharma <parth9512158@gmail.com>
2025-03-22 13:32:46 +0530

    Added new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 const nextConfig = {
-  output: 'export', // Replaces `next export`
+  reactStrictMode: false,
+  distDir: 'build', // Custom build directory instead of `.next`
+  output: 'standalone', // Standalone mode for SSR & dynamic routes
+  trailingSlash: true, // Enable trailing slash for all URLs
   images: {
     unoptimized: true, // Required if using Next.js Image component
   },
